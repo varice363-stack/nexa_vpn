b2681ea45c1453dba4ab930c7bc529d62d0ede4f
varice363 <varice363@users.noreply.github.com>
2026-09-04 01:45:36 +0000

    fix: исправление StateProvider в killswitch_providers

--- lib/providers/killswitch_providers.dart
+++ lib/providers/killswitch_providers.dart
@@ -28,6 +28,4 @@ final killSwitchInitProvider = FutureProvider<void>(
 /// Kill Switch enabled state provider.
 ///
 /// Watches the Kill Switch enabled/disabled state.
-final killSwitchEnabledProvider = StateProvider<bool>(
-  (ref) => false,
-);
+final killSwitchEnabledProvider = StateProvider<bool>((ref) => false);
